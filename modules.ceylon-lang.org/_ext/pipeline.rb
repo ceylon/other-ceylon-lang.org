0b99569ee14414933a6431432d0b1f9e899960d3
Emmanuel Bernard <emmanuel@hibernate.org>
2012-02-17 18:21:34 +0100

    Remove unused files from modules

--- modules.ceylon-lang.org/_ext/pipeline.rb
+++ modules.ceylon-lang.org/_ext/pipeline.rb
@@ -1,43 +1,12 @@
 require 'toc'
-require 'mytagger'
-require 'mypaginator'
-require 'my_tag_cloud'
-require 'authorsplitter'
-require 'multiatomizer'
-require 'mydisqus'
 require 'gsub'
 require 'deeplink'
 require 'sanitizer'
-require 'main_atomizer'
 
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::DataDir.new
-  extension Awestruct::Extensions::Posts.new( '/blog', :posts )
-  extension Awestruct::Extensions::Paginator.new(:posts, '/blog/index', :per_page => 5 )
-  extension Awestruct::Extensions::MyTagger.new( :posts, 
-                                               '/blog/index', 
-                                               '/blog/tags', 
-                                               :per_page=>5 )
-  extension Awestruct::Extensions::MyTagCloud.new( :posts, 
-                                               '/blog/tags/index.html',
-                                               :layout=>'default' )
-  # The current limitation is that the MultiAtomizer does not take array of tags into account
-  #extension Awestruct::Extensions::MultiAtomizer.new( :posts, 'tags', '/blog/tags' )
-  extension Awestruct::Extensions::AuthorSplitter.new( :posts, 
-                                              '/blog/index', 
-                                              '/blog/authors', 
-                                              :per_page=>5 )
-  extension Awestruct::Extensions::MyTagCloud.new( :posts, 
-                                                '/blog/authors/index.html',
-                                                :layout=>'default',
-                                                :title=>'Authors',
-                                                :items_property_suffix=>'authors' )
-
-  extension Awestruct::Extensions::MainAtomizer.new( :posts, '/blog/blog.atom' )
-  extension Awestruct::Extensions::MultiAtomizer.new( :posts, 'author', '/blog/authors' )
 
   extension Awestruct::Extensions::Indexifier.new
-  extension Awestruct::Extensions::MyDisqus.new
   helper Awestruct::Extensions::Partial
   extension TOC.new(:levels => 2)
 
